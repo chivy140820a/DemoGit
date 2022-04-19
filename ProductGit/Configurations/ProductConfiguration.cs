113fa53a7b9c1cc86624543f094f375fdcba9211
chivyh1408@gmail.com <chivy1408@gmail.com>
2022-04-19 22:58:32 +0700

    init

---
+++ ProductGit/Configurations/ProductConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductGit.Entity;
+
+namespace ProductGit.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
